f479a2fc19bcce40078468dd1bc0adb4af0f0308
GreySeekerx <utopek94@gmail.com>
2025-08-12 14:49:32 +0200

    changes

--- src/textnode.py
+++ src/textnode.py
@@ -3,8 +3,8 @@ from enum import Enum
 class TextType(Enum):
     TEXT = "text"
     BOLD_TEXT = "bold"
-    ITALIC_TEXT = "italic"
-    CODE_TEXT = "code"
+    ITALIC = "italic"
+    CODE = "code"
     LINK = "link"
     IMAGE = "image"
 
