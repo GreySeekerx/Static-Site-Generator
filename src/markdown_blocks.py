4e18c5723aaaf2530997c1ef2b51606a7d236825
GreySeekerx <utopek94@gmail.com>
2025-08-30 23:07:55 +0200

    clean

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -137,7 +137,4 @@ def quote_to_html_node(block):
     children = text_to_children(content)
     return ParentNode("blockquote", children)
     
-                 #ddd
-                 #fff
-                 #hghj
-                 #ekfdm
+                
